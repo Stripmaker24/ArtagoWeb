d7fee0cee727b957c6c4fcce4207c1f06aa69762
Stripmaker24 <michelle.stripmaker24@hotmail.com>
2024-12-07 15:11:30 +0100

    WIP
    adding bas stylesheet and starting on navbar switching from laptop to PC

--- ArtagoWebsite/Website.client/src/main.tsx
+++ ArtagoWebsite/Website.client/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import React from 'react'
+import { ThemeProvider } from '@emotion/react'
+import Theme from './utilities/Theme.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+    <StrictMode>
+        <ThemeProvider theme={Theme}>
+            <App />
+        </ThemeProvider>
   </StrictMode>,
 )
---
+++ ArtagoWebsite/Website.client/src/utilities/Theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+    palette: {
+        primary: {
+            main: '#224229',
+        },
+        secondary: {
+            main: '#DCC5A5',
+        },
+        info: {
+            main: '#216516',
+        }
+    },
+    typography: {
+        fontFamily: 'Bookman Old Style',
+    }
+})
